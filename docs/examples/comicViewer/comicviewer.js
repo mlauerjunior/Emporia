5f6eec3d24785425eb2f2b54f69c964b7ae4a7b7
Michael Lauer <Michael.Lauer@csgi.com>
2019-01-21 20:43:43 -0600

    comic viewer and main page updates
    cpmic

--- docs/examples/comicViewer/comicviewer.js
+++ docs/examples/comicViewer/comicviewer.js
@@ -1,5 +1,42 @@
-$("#flipbook").turn({
-  width: 1000,
-  height: 710,
-  // autoCenter: true
-});
+var pswpElement = document.querySelectorAll('.pswp')[0];
+
+// build items array
+var items = [
+    {
+        src: '../carousel/Evaluation-1.png',
+        w: 1200,
+        h: 900
+    },
+    {
+        src: '../carousel/Evaluation-2.png',
+        w: 1200,
+        h: 900
+    },
+        {
+        src: '../carousel/Evaluation-3.png',
+        w: 1200,
+        h: 900
+    },
+    {
+        src: '../carousel/Evaluation-4.png',
+        w: 1200,
+        h: 900
+    },
+    {
+        src: '../carousel/Evaluation-5.png',
+        w: 1200,
+        h: 900
+    },
+
+];
+
+// define options (if needed)
+var options = {
+    // optionName: 'option value'
+    // for example:
+    index: 0 // start at first slide
+};
+
+// Initializes and opens PhotoSwipe
+var gallery = new PhotoSwipe( pswpElement, PhotoSwipeUI_Default, items, options);
+gallery.init();
