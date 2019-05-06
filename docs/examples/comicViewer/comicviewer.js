41d6e5f3d0ab51050f793c25da69c0c0467d2b58
Michael Lauer <Michael.Lauer@csgi.com>
2019-05-06 11:54:22 -0500

    comic viewer and mobile
    comic viewer

--- docs/examples/comicViewer/comicviewer.js
+++ docs/examples/comicViewer/comicviewer.js
@@ -1,64 +1,18 @@
-var pswpElement = document.querySelectorAll('.pswp')[0];
+(function() { 
+    G.init();
+    Galleria.loadTheme('galleria/themes/classic/galleria.classic.min.js');
+    Galleria.configure({
+        transition: 'fade',
+        imageCrop: true,
+        lightbox: true,
+    });
+    Galleria.ready(function() {
+        var gallery = this; // galleria is ready and the gallery is assigned
+          $('#fullscreen').click(function() {
+            gallery.toggleFullscreen(); // toggles the fullscreen
+          });
+    });
+    Galleria.run('.galleria');
+}());
 
 
-// build items array
-var items = [
-    {
-        src: '../carousel/Evaluation-1.png',
-        w: 1000,
-        h: 800
-    },
-    {
-        src: '../carousel/Evaluation-2.png',
-        w: 1000,
-        h: 800
-    },
-        {
-        src: '../carousel/Evaluation-3.png',
-        w: 1000,
-        h: 800
-    },
-    {
-        src: '../carousel/Evaluation-4.png',
-        w: 1000,
-        h: 800
-    },
-    {
-        src: '../carousel/Evaluation-5.png',
-        w: 1000,
-        h: 800
-    },
-
-];
-var template = document.getElementById("galleryPSWP"); // .pswp
-
-// define options (if needed)
-var options = {
-    modal: false,
-    closeOnScroll: false,
-    index: 0,
-    getThumbBoundsFn: function(index) {
-            // var obj = document.getElementById("galleryPSWP");
-            // var rect = obj.getBoundingClientRect();
-            // console.log(rect);
-
-        var rect = {x: 0,  y: 0 , w: 0}
-        var templateBounds = template.parentElement.getBoundingClientRect();
-        rect.x -= templateBounds.left;
-        rect.y -= templateBounds.top;
-
-        console.log(rect);
-        return rect;
-    }
-};
-
-
-
-// Initializes and opens PhotoSwipe
-var photoSwipe = new PhotoSwipe( pswpElement, PhotoSwipeUI_Default, items, options, template);
-photoSwipe.listen('updateScrollOffset', function(_offset) {
-    var r = template.getBoundingClientRect();
-    _offset.x += r.left;
-    _offset.y += r.top;
-});
-photoSwipe.init();
